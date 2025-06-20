1b37082726793bb0a6896320236f35f464002dba
konstantinos <konstgram@gmail.com>
2025-06-20 22:19:04 +0300

    Added option of constant learning late.

--- headers/optimizer.h
+++ headers/optimizer.h
@@ -7,9 +7,19 @@
 #include"linearAlgebra.h"
 #include<stdlib.h>
 
+#define DYNAMIC_STEP 1
+#define CONSTANT_STEP 2
+
+/**
+ * The optimizer has the role of handling the weights and biases 
+ * and applying a gradient step to them accordingly,
+ * in order to approach the steepest descent.
+ * 
+*/
+
 typedef struct optimizer
 {
-    int k;
+    int k, step_rule;
     double lr;
 }Optimizer;
 
@@ -17,6 +27,8 @@ Optimizer* createOptimizer() {
     Optimizer* opt = (Optimizer*)malloc(sizeof(Optimizer));
     opt->k=1;
     opt->lr=0.9;
+
+    opt->step_rule = DYNAMIC_STEP;
 }
 
 void dynamic_sgd(module* nn, Optimizer* opt){
@@ -37,11 +49,13 @@ void dynamic_sgd(module* nn, Optimizer* opt){
             return;
         }
 
+        /* this assertion seems reduntant */
         assert(iterator->prev->id!=START_NODE_ID);
+        
+        /***** weights ********/
         gradW = (Matrix*) cur_layer->gradW;
-
         printf("\n[OPTIMIZER] NORM of gradient %f\n",norm(cur_layer->gradW,2));
-        if(norm(gradW,2)>0)
+        if(norm(gradW,2)>0 && opt->step_rule = DYNAMIC_STEP)
             opt->lr = (double) 1/(opt->k * norm(gradW, 2));
         printf("[OPTIMIZER]lr=%f\n", opt->lr);
         weights_tmp = cur_layer->weights;
@@ -52,13 +66,13 @@ void dynamic_sgd(module* nn, Optimizer* opt){
         
         /****** biases ******/
         gradb = (Matrix*) cur_layer->gradb;
-        if(norm(gradb,2)>0)
+        if(norm(gradb,2)>0 && opt->step_rule = DYNAMIC_STEP)
             opt->lr = (double) 1/(opt->k * norm(gradb, 2));
         biases_tmp=cur_layer->biases;
         cur_layer->biases = linearCombination(biases_tmp, gradb, (double) 1, (double) -opt->lr);
         cur_layer->biases = linearCombination(cur_layer->biases, zeros_like(cur_layer->biases), (double)1/norm(cur_layer->biases, 2), 0);
 
-        //
+        /* go to previous layer */
         iterator=iterator->prev;
 
     }
